94d744d870450c2249622d499505905c2d546d24
Nolan Boate <103973431+ImonaBOATE@users.noreply.github.com>
2022-07-20 23:52:36 -0700

    Add files via upload
    second commit

--- combinePDF.py
+++ combinePDF.py
@@ -2,12 +2,14 @@
 import PyPDF2 # manipulates PDFs
 import os
 
+merger = PyPDF2.PdfFileMerger()
 
 for file in os.listdir(os.curdir):
     if file.endswith('.pdf'):
-        PyPDF2.PdfFileMerger().append(file)
-    # creating the new combined PDF file and storing it in the current folder
-    PyPDF2.PdfFileMerger().write('combined.pdf')
+        merger.append(file)
+        print(file)
+# creating the new combined PDF file and storing it in the current folder
+merger.write('combined.pdf')
 
 
 
